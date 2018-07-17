49db56362b778902390c48638a1bfd602315762f
riphord <vlad.gorenkov.20@gmail.com>
2018-07-17 15:04:08 +0200

    HW3 complement bd scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -14,12 +15,23 @@ CREATE TABLE users
   enabled          BOOL DEFAULT TRUE       NOT NULL,
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  description VARCHAR NOT NULL,
+  calories    INTEGER NOT NULL,
+  date_time   TIMESTAMP           DEFAULT now(),
+  user_id     INTEGER NOT NULL ,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,14 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, description, calories, date_time,user_id) VALUES
+  (100002, 'Завтрак', 500, TIMESTAMP '2015-05-30 10:00:00',100000),
+  (100003, 'Обед', 1000, TIMESTAMP '2015-05-30 13:00:00',100000),
+  (100004, 'Ужин', 500, TIMESTAMP '2015-05-30 20:00:00',100000),
+  (100005, 'Завтрак', 1000, TIMESTAMP '2015-05-31 10:00:00',100000),
+  (100006, 'Обед', 500, TIMESTAMP '2015-05-31 13:00:00',100000),
+  (100007, 'Ужин', 510, TIMESTAMP '2015-05-31 20:00:00',100000),
+  (100008, 'Админ ланч', 510, TIMESTAMP '2015-06-1 14:00:00',100001),
+  (100009, 'Админ ужин', 1500, TIMESTAMP '2015-06-1 21:00:00',100001);
+
